a1ee603da2b00136340dc7d19d8e0192b4abcb5d
zacdevil10 <zac1999ad@gmail.com>
2016-06-01 16:01:36 +0100

    applied layout to settings page, updated logo (might change)

--- mobile/src/main/java/uk/co/appsbystudio/geoshare/settings/SettingsFragment.java
+++ mobile/src/main/java/uk/co/appsbystudio/geoshare/settings/SettingsFragment.java
@@ -1,11 +1,17 @@
 package uk.co.appsbystudio.geoshare.settings;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.ContextThemeWrapper;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import uk.co.appsbystudio.geoshare.MainActivity;
 import uk.co.appsbystudio.geoshare.R;
 
 public class SettingsFragment extends Fragment {
@@ -14,6 +20,23 @@ public class SettingsFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        final Context context = new ContextThemeWrapper(getActivity(), R.style.test_theme);
+        LayoutInflater layoutInflater = inflater.cloneInContext(context);
+        View view = layoutInflater.inflate(R.layout.fragment_settings, container, false);
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        toolbar.setNavigationIcon(R.drawable.ic_menu_white_24dp);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity) getActivity()).openDrawer();
+            }
+        });
+        toolbar.setTitle(R.string.settings);
+
+        return view;
     }
 }
